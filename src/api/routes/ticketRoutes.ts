7a152a10cf264ab4a3f5cc25231cb6ed5da900bc
itaji-create <itaji.1999@gmail.com>
2024-07-06 03:46:46 -0300

    feat: adiciona o middleware do celebrate com o schema do body de ticket

--- src/api/routes/ticketRoutes.ts
+++ src/api/routes/ticketRoutes.ts
@@ -1,12 +1,21 @@
 import express from 'express';
 import * as TicketController from '../controllers/ticketController';
+import { celebrate, Joi, Segments } from 'celebrate';
 
 const router = express.Router();
-
+const schema = {
+  [Segments.BODY]: Joi.object({
+    chair: Joi.string()
+      .pattern(/^[a-z]\d$/)
+      .required(),
+    value: Joi.number().integer().positive().required(),
+  }),
+};
 router.get('/tickets', TicketController.getTickets);
 //router.get('/tickets/:id', TicketController.getTicketById);
 router.post(
   '/movies/:movie_id/sessions/:session_id/tickets',
+  celebrate(schema),
   TicketController.createTicket,
 );
 //router.put('/tickets/:id', TicketController.updateTicket);
