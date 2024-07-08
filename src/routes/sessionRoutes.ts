ca5e8606260c7b5e8cbc8f4e7165d8189d927be5
itaji-create <itaji.1999@gmail.com>
2024-07-08 14:16:49 -0300

    feat: adiciona a validação do body de session

--- src/routes/sessionRoutes.ts
+++ src/routes/sessionRoutes.ts
@@ -1,16 +1,24 @@
+import { celebrate } from 'celebrate';
 import { Router } from 'express';
 import {
   createSession,
   updateSession,
   deleteSession,
-  getSession,
 } from 'src/api/controllers/sessionController';
+import { sessionSchema } from './schemas/sessionSchema';
 
 const router = Router();
 
-router.get('/sessions', getSession);
-router.post('/movies/:movie_id/sessions', createSession);
-router.put('/movies/:movie_id/sessions/:id', updateSession);
+router.post(
+  '/movies/:movie_id/sessions',
+  celebrate(sessionSchema),
+  createSession,
+);
+router.put(
+  '/movies/:movie_id/sessions/:id',
+  celebrate(sessionSchema),
+  updateSession,
+);
 router.delete('/movies/:movie_id/sessions/:id', deleteSession);
 
 export default router;
